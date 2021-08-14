49d879bdab70ebf8760f7899dbc6b38b2f58c51b
Juan Camilo Arboleda Garcia <camiloarboleda2000@gmail.com>
2021-08-14 15:12:52 -0500

    Initial commit

---
+++ WEBAPI2/Controllers/NewsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+
+
+namespace WEBAPI2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NewsController : ControllerBase
+    {
+        [HttpGet]
+
+        public string Get(string keyword)
+        {
+            using (WebClient web = new WebClient())
+            {
+                string url = string.Format("");
+                if (keyword == null || keyword.Equals(String.Empty))
+                {
+                    keyword = "bogota";
+                    url = string.Format("https://newsapi.org/v2/everything?" + "q=" + keyword + "&" + "from=2021-08-12&" + "sortBy=popularity&" + "apiKey=6363f918e6b14682b34d195e123da61a");
+
+                }
+
+                else
+                {
+                    url = string.Format("https://newsapi.org/v2/everything?" + "q=" + keyword + "&" + "from=2021-08-12&" + "sortBy=popularity&" + "apiKey=6363f918e6b14682b34d195e123da61a");
+
+                }
+                var json = web.DownloadString(url);
+                return json;
+
+            }
+        }
+    }
+}
---
+++ WEBAPI2/Controllers/WeatherController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace WEBAPI2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeatherController : ControllerBase
+    {
+
+        [HttpGet]
+        
+        public string Get(string ciudad)
+        {
+            using(WebClient web=new WebClient())
+            {
+                string url = string.Format("");
+                if (ciudad == null || ciudad.Equals(String.Empty))
+                {
+                    ciudad = "cali";
+                    url = string.Format("http://api.openweathermap.org/data/2.5/weather?q=" + ciudad + "&appid=d2e612eb36729d83d1dd24b0a4be17d2");
+
+                }
+
+                else
+                {
+                    url = string.Format("http://api.openweathermap.org/data/2.5/weather?q=" + ciudad + "&appid=d2e612eb36729d83d1dd24b0a4be17d2");
+
+                }
+                var json = web.DownloadString(url);
+                return json;
+
+            }
+            
+
+
+
+            //string url = @"api.openweathermap.org/data/2.5/weather?q=california&appid=d2e612eb36729d83d1dd24b0a4be17d2";
+            //WebRequest webRequest = WebRequest.Create(url);
+            //HttpWebRequest httpwebRequest = null;
+            //httpwebRequest = (HttpWebRequest)webRequest.GetResponse();
+        }
+    }
+}
