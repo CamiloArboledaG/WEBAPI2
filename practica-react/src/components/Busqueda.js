ac60231a58250883ed678e7c3a684ea4a0714057
Juan Camilo Arboleda Garcia <camiloarboleda2000@gmail.com>
2021-08-14 16:30:31 -0500

    Update Busqueda.js

--- practica-react/src/components/Busqueda.js
+++ practica-react/src/components/Busqueda.js
@@ -1,55 +1,76 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
-class Busqueda extends Component{
+class Busqueda extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state={Weather:[]}
-      }
+        this.state = { Weather: [] }
+    }
 
-      getWeatherState = response_data =>{
-          return "SUN"
-      }
+    getWeatherState = response_data => {
+        return "SUN"
+    }
 
-      getData= response_data => {
-          const {humidity, temp} =response_data.main;
-          const {speed} = response_data.wind;
-          const weatherStateTemp = this.getWeatherState(response_data);
+    getData = response_data => {
+        var today = new Date();
+        var hour = today.getHours() + ":" + today.getMinutes();
 
-          const data = {
-              humidity,
-              temp,
-              weatherState: weatherStateTemp,
-              wind: `${speed} m/s`
-          }
-          return data;
-      }
-    
-      refreshList(){
-        fetch(process.env.REACT_APP_API+'Weather')
-        .then(response=>response.json())
-        .then(data => {
-            const newData = this.getData(data);
-            this.setState({Weather: newData})
-        })
-      }
+        const { humidity, temp, pressure, feels_like } = response_data.main;
+        const feels_likeFloat = parseFloat(feels_like);
+        const feels = feels_likeFloat - 273.15;
+        const tempfloat = parseFloat(temp);
+        const temperatura = tempfloat - 273.15;
 
-      componentDidMount(){
-          this.refreshList();
-      }
+        const { speed, deg } = response_data.wind;
+        const weatherStateTemp = this.getWeatherState(response_data);
+        const { description } = response_data.weather[0];
+        const name = response_data.name;
 
-      componentDidUpdate(){
-          this.refreshList();
-      }
+        const data = {
+            hour,
+            temperatura,
+            description,
+            wind: `${speed} m/s`,
+            deg,
+            pressure,
+            humidity,
+            feels,
+            name,
+
+            
+            
+
+            weatherState: weatherStateTemp,
+            
+        }
+        return data;
+    }
+
+    refreshList() {
+        fetch(process.env.REACT_APP_API + 'Weather')
+            .then(response => response.json())
+            .then(data => {
+                const newData = this.getData(data);
+                this.setState({ Weather: newData })
+            })
+    }
+
+    componentDidMount() {
+        this.refreshList();
+    }
+
+    componentDidUpdate() {
+        this.refreshList();
+    }
 
 
-    render(){
+    render() {
 
-        const {Weather} =this.state;
-        
-        return(
+        const { Weather } = this.state;
+
+        return (
             <div>
-                {Weather.wind}
+                {Weather.hour}
             </div>
         )
     }
